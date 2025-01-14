f34ef4b16c0c2490d09560772bacaa6556e8bd1d
ASUNG0401 <ksw3157917@gmail.com>
2025-01-14 21:41:58 +0900

    1

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
-import discord, random
+import discord
 from discord.ext import commands
-bot = commands.Bot(command_prefix="$", intents=discord.Intents.all())
-TOKEN = "(아까 복사한 토큰 입력)"
+from discord import app_commands
 
-# 로그인
-@bot.event
-async def on_ready():
-    print(f"{bot.user.name} 로그인 성공")
-    await bot.change_presence(status=discord.Status.online, activity=discord.Game('테스트'))
+class Client(commands.Bot):
+    async def on_ready(self):
+        print(f'온라인 됨 {self.user}!')
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return 
+        #만약 메세지의 내용이 ('ㄲㅈ')로 시작한다면, 그 채널에 메세지 전송 sout같은 느낌 
+        if message.content.startswith('ㄲㅈ'): 
+            await message.channel.send(f'{message.author}님의 비속어 감지!')
+        if message.content.startswith('ㅅㅂ'):
+            await message.channel.send(f'{message.author}님의 비속어 감지!')     
+        if message.content.startswith('시발'):
+            await message.channel.send(f'{message.author}님의 비속어 감지!')       
+        if message.content.startswith('병신'):
+            await message.channel.send(f'{message.author}님의 비속어 감지!')
+        if message.content.startswith('ㅈ같네'):
+            await message.channel.send(f'{message.author}님의 비속어 감지!')
+
+    # async def on_reaction_add(self, reaction, user):
+    #     await reaction.message.channel.send('반응함 :)')  
+
     
-# 인사
-@bot.command(name="안녕", help="인사말", aliases=["인사", "하이"])
-async def hello(ctx):
-  hi = random.randrange(1,4)
-  if hi == 1:
-    await ctx.channel.send("안녕하세요")
-  elif hi == 2:
-    await ctx.channel.send("안녕")
-  elif hi == 3:
-    await ctx.channel.send("네, 안녕하세요")
 
-# 봇 작동
-bot.run(TOKEN)
+intents = discord.Intents.default()
+intents.message_content = True
+client = Client(command_prefix="!", intents = intents) 
+
+#슬래시 코맨드 작성법 ex) !음악 !스킵 (슬래시 코맨드란 !해서 입력하는 코맨드 명령어)
+@client.tree.command(name = "hello", description="say hello")  #무슨 커맨드인지 이거로 예시를 들자면 !hello 하고 밑에 설명에 무슨 커맨드인지 description
+async def sayHello(interaction: discord.Interaction):
+    await interaction.response.send_message("hi there!") #
+    
+client.run('')
